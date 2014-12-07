4ce4b3b3a86d35cd0b9f088179b4d8f921fe241c
Dominic Lacaille <ldom66@gmail.com>
2014-12-07 14:51:44 -0500

    - Now possible to observe properties changed in Dock Settings.

--- CleverDock/CleverDock/Model/DockSettings.cs
+++ CleverDock/CleverDock/Model/DockSettings.cs
@@ -1,17 +1,35 @@
 ﻿using CleverDock.Managers;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CleverDock.Model
 {
-    public class DockSettings
+    public class DockSettings : INotifyPropertyChanged
     {
-        public const string SETTINGS_VERSION = "0.2.0";
+        public const string SETTINGS_VERSION = "0.3.0";
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
 
         public static DockSettings DefaultDockSettings = new DockSettings
         {
             IconSize = 48,
             IconPadding = 20,
             CollapseDuration = 0.2,
+            DockHideDuration = 0.6,
+            DockShowDuration = 0.3,
+            DockHideDelay = 300,
+            DockShowDelay = 50,
             SaveAutomatically = true,
             ReserveScreenSpace = false,
             Icons = new List<SerializableIconInfo>(),
@@ -19,17 +37,86 @@ namespace CleverDock.Model
         };
 
         public string Version = SETTINGS_VERSION;
-        public double CollapseDuration;
-        public int IconPadding;
-        public int IconSize;
-        public Theme Theme;
-        public List<SerializableIconInfo> Icons;
-        public bool SaveAutomatically;
-        public bool ReserveScreenSpace;
+
+        private double _collapseDuration;
+        public double CollapseDuration
+        {
+            get { return _collapseDuration; }
+            set { _collapseDuration = value; OnPropertyChanged(); }
+        }
+
+        private double _dockHideDuration;
+        public double DockHideDuration
+        {
+            get { return _dockHideDuration; }
+            set { _dockHideDuration = value; OnPropertyChanged(); }
+        }
+        private double _dockShowDuration;
+        public double DockShowDuration
+        {
+            get { return _dockShowDuration; }
+            set { _dockShowDuration = value; OnPropertyChanged(); }
+        }
+        private double _dockHideDelay;
+        public double DockHideDelay
+        {
+            get { return _dockHideDelay; }
+            set { _dockHideDelay = value; OnPropertyChanged(); }
+        }
+        private double _dockShowDelay;
+        public double DockShowDelay
+        {
+            get { return _dockShowDelay; }
+            set { _dockShowDelay = value; OnPropertyChanged(); }
+        }
+
+        private int _iconPadding;
+        public int IconPadding
+        {
+            get { return _iconPadding; }
+            set { _iconPadding = value; OnPropertyChanged(); }
+        }
+
+        private int _iconSize;
+        public int IconSize
+        {
+            get { return _iconSize; }
+            set { _iconSize = value; OnPropertyChanged(); }
+        }
+
+        private Theme _theme;
+        public Theme Theme
+        {
+            get { return _theme; }
+            set { _theme = value; OnPropertyChanged(); }
+        }
+
+        private List<SerializableIconInfo> _icons;
+        public List<SerializableIconInfo> Icons
+        {
+            get { return _icons; }
+            set { _icons = value; OnPropertyChanged(); }
+        }
+
+        private bool _saveAutomatically;
+        public bool SaveAutomatically
+        {
+            get { return _saveAutomatically; }
+            set { _saveAutomatically = value; OnPropertyChanged(); }
+        }
+
+        private bool _reserveScreenSpace;
+        public bool ReserveScreenSpace
+        {
+            get { return _reserveScreenSpace; }
+            set {  _reserveScreenSpace = value; OnPropertyChanged(); }
+        }
 
         public int OuterIconSize
         {
             get { return IconSize + IconPadding; }
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
